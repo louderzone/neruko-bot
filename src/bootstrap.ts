340a28119685451a4b6f96dee1c61e2d9b157d4b
Eric Lam <ericlamyauping@gmail.com>
2020-02-20 16:54:25 +1000

    setup discord login

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,1 +1,7 @@
-console.log("hello world!");
+import { Client } from "discord.js";
+
+const client = new Client();
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+client.login(process.env.DISCORD_TOKEN);
