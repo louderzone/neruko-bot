591267a886b9ad86aab60b98b4d96611c474167a
Eric Lam <ericlamyauping@gmail.com>
2020-02-21 10:46:24 +1000

    fix bot not logged in before receiving the request when used in Azure web app

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,21 +3,29 @@ import express = require("express");
 import { Request, Response } from "express";
 import bodyParser = require("body-parser");
 
-const bot = new Client();
-bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
-});
-bot.login(process.env.DISCORD_TOKEN);
+async function init(): Promise<void> {
+    const bot = new Client();
+    bot.on('ready', () => {
+        console.log(`Logged in as ${bot.user.tag}!`);
+    });
+    // Make sure Discord bot is logged in before anything.
+    await bot.login(process.env.DISCORD_TOKEN);
 
-// Starts webserver
-const app = express();
-app.use(bodyParser.json());
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello Happy World :) - Neruko~~");
-});
-app.post('/shift/announce', (req: Request, res: Response) => {
-    const channels = bot.channels as Collection<string, TextChannel>;
-    channels.find(`name`, process.env.CHANNEL_NAME).send(req.body.msg);
-    res.send(200);
-});
-app.listen(process.env.PORT, () => console.log("Started"));
+    // Starts webserver
+    const app = express();
+    app.use(bodyParser.json());
+    app.get("/", (req: Request, res: Response) => {
+        res.send("Hello Happy World :) - Neruko~~");
+    });
+    app.post('/shift/announce', (req: Request, res: Response) => {
+        const channels = bot.channels as Collection<string, TextChannel>;
+        const talkChannels =  channels.filter((c) => c.name === process.env.CHANNEL_NAME);
+        talkChannels.forEach(c => {
+            c.send(req.body.msg);
+        });
+        res.send(200);
+    });
+    app.listen(process.env.PORT, () => console.log("Started"));
+}
+
+init();
