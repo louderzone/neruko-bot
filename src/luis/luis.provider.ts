cd74326824cda3ea8fd2f781ccc3560c309988d2
Eric Lam <ericlamyauping@gmail.com>
2020-03-26 17:06:29 +1000

    replace `botbuilder-ai` lib with `luis-sdk-async` as it only works for bot framework

--- src/luis/luis.provider.ts
+++ src/luis/luis.provider.ts
@@ -1,28 +1,28 @@
 import { container } from "../inversify.config";
-import { LuisRecognizer } from 'botbuilder-ai';
 import { PROVIDER } from "../constants/providers";
+import Luis = require("luis-sdk-async");
 
 /**
  * Creates the instance of luis recognizer
  */
-const luisRecognizer = new LuisRecognizer({
-    applicationId: process.env.LUIS_APP_ID,
-    endpointKey: process.env.LUIS_AUTHORING_KEY,
-    endpoint: process.env.LUIS_END_POINT
-});
+const luisRecognizer = new Luis(
+    process.env.LUIS_APP_ID,
+    process.env.LUIS_AUTHORING_KEY,
+    process.env.LUIS_END_POINT
+);
 
 /**
  * Provide LUIS Recognizer in singleton manner
  */
 function provideLuisRecognizer() {
-    return (): Promise<LuisRecognizer> => {
-        return new Promise<LuisRecognizer>(async () => {
+    return (): Promise<Luis> => {
+        return new Promise<Luis>(async () => {
             return luisRecognizer
         });
     }
 }
 
 container.bind<LuisRecognizerProvider>(PROVIDER.LuisRecognizer)
-    .toProvider<LuisRecognizer>(provideLuisRecognizer);
+    .toProvider<Luis>(provideLuisRecognizer);
 
-export type LuisRecognizerProvider = () => Promise<LuisRecognizer>;
+export type LuisRecognizerProvider = () => Promise<Luis>;
