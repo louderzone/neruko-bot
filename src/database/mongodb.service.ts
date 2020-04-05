fbfa6dab1a803fe5960ec07606861af7532dbfa2
Eric Lam <ericlamyauping@gmail.com>
2020-04-05 18:47:49 +1000

    Adds mongodb service

---
+++ src/database/mongodb.service.ts
@@ -0,0 +1,37 @@
+import { fluentProvide } from "inversify-binding-decorators";
+import { Collection, Db, MongoClient } from "mongodb";
+import { SERVICE } from "../constants/services";
+
+// Connection URL
+const uri = process.env.MONGO_CONNECTION_STRING;
+
+// Database Name
+const dbName = process.env.MONGO_DBNAME;
+
+/**
+ * Represents to MongoDb connection
+ */
+@fluentProvide(SERVICE.MongoDb)
+    .inSingletonScope()
+    .done()
+export class MongoDb {
+    private db: Db;
+
+    constructor() {
+        // Use connect method to connect to the server
+        const client = new MongoClient(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        client.connect(() => {
+            this.db = client.db(dbName);
+        });
+    }
+
+    /**
+     * Gets the user collection
+     */
+    getUsers(): Collection {
+        return this.db.collection("users");
+    }
+}
