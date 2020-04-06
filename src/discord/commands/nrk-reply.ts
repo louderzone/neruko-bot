862c339b7fac7e1a8f49a7b7c52cc05db9791d0e
Eric Lam <ericlamyauping@gmail.com>
2020-04-06 13:01:37 +1000

    documentation

--- src/discord/commands/nrk-reply.ts
+++ src/discord/commands/nrk-reply.ts
@@ -2,6 +2,11 @@ import { Message } from "discord.js";
 
 export const REPLY_COMMAND = "/nrk:reply ";
 
+/**
+ * Handles when /nrk:reply command is received
+ *
+ * @param msg The discord message received
+ */
 export async function nrkReply(msg: Message): Promise<void> {
     msg.delete();
     await msg.channel.send(msg.content.substring(REPLY_COMMAND.length));
