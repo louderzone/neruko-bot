479caba75990aa86325a58dc2db40ac369dc86b8
Eric Lam <ericlamyauping@gmail.com>
2020-04-06 18:21:13 +1000

    load providers before any controllers to avoid dependencies not being loaded

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -2,8 +2,8 @@ import { Container } from "inversify";
 
 export const container = new Container();
 
-// Load Express controllers
-import "./express/controllers.config";
-
 // Load Providers
 import "./luis/luis.service";
+
+// Load Express controllers
+import "./express/controllers.config";
