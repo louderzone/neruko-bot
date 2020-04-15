4564f802185de411fdbdcea623805614ecbe669b
Eric Lam <ericlamyauping@gmail.com>
2020-04-16 01:28:03 +1000

    fix wrong reference / add documentation

--- src/utils/filters/not-undefined-filter.ts
+++ src/utils/filters/not-undefined-filter.ts
@@ -1,5 +1,12 @@
-import { KeyValueObject } from "misc/key-value-pair";
+import { KeyValueObject } from "../../misc/key-value-pair";
 
+/**
+ * Filters key value pairs with value undefined
+ * when used with Array.Filter
+ *
+ * @param pair
+ * @see Array.Filter
+ */
 export function notUndefined(pair: KeyValueObject<object>): boolean {
     return pair.value !== undefined;
 }
